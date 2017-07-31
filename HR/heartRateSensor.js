148e70f4bf58f3b5144f8c1c2aaa33bf06c1e668
Esteban <estebaniglesiasmanriquez@gmail.com>
2017-07-31 02:40:45 -0400

    accept all devices

--- HR/heartRateSensor.js
+++ HR/heartRateSensor.js
@@ -8,7 +8,7 @@
       this._characteristics = new Map();
     }
     connect() {
-      return navigator.bluetooth.requestDevice({filters:[{services:[ 'heart_rate' ]}]})
+      return navigator.bluetooth.requestDevice({"acceptAllDevices":true})
       .then(device => {
         this.device = device;
         return device.gatt.connect();
